d8dfe070aa23f0ded9bf1d69fbcd2934f80c48f1
Josh Hansen <jhansen7325@gmail.com>
2025-08-09 12:35:37 -0700

    Add validations and allowed statuses to Ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,7 @@
 class Ticket < ApplicationRecord
+  STATUSES = %w[open in_progress resolved closed].freeze
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :status, presence: true, inclusion: { in: STATUSES }
 end
